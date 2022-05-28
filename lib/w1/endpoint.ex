867cd5605368ed69c4878cc10193de15fae56829
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2022-05-28 14:16:30 +0300

    add ingester to docker compose

--- lib/w1/endpoint.ex
+++ lib/w1/endpoint.ex
@@ -3,6 +3,7 @@ defmodule W1.Endpoint do
   use Plug.ErrorHandler
   require Logger
 
+  plug Plug.Logger
   plug :match
   plug Plug.Parsers, parsers: [:json], pass: ["application/json"], json_decoder: Jason
   plug :dispatch
